70d7deba4d441c63d02077e46b2d85e94a143fc9
vagrant <vagrant@homestead>
2017-09-24 16:47:30 +0000

    modify

--- fc/systemdata/data/cache/listcache/b0/e0/72/b0e0727b9c739c0fb49e8bccbac035ca.php
+++ fc/systemdata/data/cache/listcache/b0/e0/72/b0e0727b9c739c0fb49e8bccbac035ca.php
@@ -1,2 +1,2 @@
 <?php exit('dedecms');?>
-a:2:{s:4:"data";a:1:{s:2:"dd";s:2:"11";}s:7:"timeout";i:1497358822;}
+a:2:{s:4:"data";a:1:{s:2:"dd";s:2:"11";}s:7:"timeout";i:1506268537;}
--- fc/systemdata/data/common.inc.php
+++ fc/systemdata/data/common.inc.php
@@ -3,8 +3,8 @@
 $cfg_dbtype = 'mysql';
 $cfg_dbhost = 'localhost';
 $cfg_dbname = 'fc';
-$cfg_dbuser = 'root';
-$cfg_dbpwd = '';
+$cfg_dbuser = 'debian-sys-maint';
+$cfg_dbpwd = 'zQeXu4P3q5RtYmqw';
 $cfg_dbprefix = 'fc_';
 $cfg_db_language = 'utf8';
 
